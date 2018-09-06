62e0b5c7749835ce9401fea1212231108877c7c2
Silas Oliveira <soliveira@autodoc.com.br>
2018-09-06 14:41:45 -0300

    Initial commit

---
+++ Cad/app/src/main/java/com/oliveira/silas/cad/domain/Test.kt
@@ -0,0 +1,13 @@
+package com.oliveira.silas.cad.domain
+
+class Test {
+    private val name: String
+
+    constructor() {
+
+    }
+
+    constructor(name: String) {
+        this.name = name
+    }
+}
