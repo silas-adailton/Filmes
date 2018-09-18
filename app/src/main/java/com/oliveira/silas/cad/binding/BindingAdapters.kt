06ca237fcbd53a558e0b5f8a94c49c32dd3450e1
Silas Adailton de Oliveira <silasadailton@hotmail.com>
2018-09-18 01:20:51 -0300

    Lista de filmes

--- app/src/main/java/com/oliveira/silas/cad/binding/BindingAdapters.kt
+++ app/src/main/java/com/oliveira/silas/cad/binding/BindingAdapters.kt
@@ -4,22 +4,37 @@ import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.oliveira.silas.cad.ui.main.movie.MovieAdapter
 import com.oliveira.silas.domain.user.User
 import com.oliveira.silas.cad.ui.main.user.UserAdapter
+import com.oliveira.silas.domain.movies.Movie
 
 object bindAdapters {
     @JvmStatic
     @BindingAdapter("loadRecyclerView")
     fun loadRecyclerView(recyclerView: RecyclerView, listUser: List<User>?) {
         val viewAdapter: RecyclerView.Adapter<UserAdapter.ViewHolder>
-        recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+        recyclerView.layoutManager = GridLayoutManager(recyclerView.context)
         recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL))
         recyclerView.setHasFixedSize(true)
 
         viewAdapter = UserAdapter(listUser!!)
+        recyclerView.adapter = viewAdapter
+    }
+
+    @JvmStatic
+    @BindingAdapter("loadRecyclerViewMovies")
+    fun loadRecyclerViewMovies(recyclerView: RecyclerView, listMovies: List<Movie>?) {
+        val viewAdapter: RecyclerView.Adapter<MovieAdapter.ViewHolder>
+        recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+        recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL))
+        recyclerView.setHasFixedSize(true)
+
+        viewAdapter = MovieAdapter(listMovies!!)
         recyclerView.adapter = viewAdapter
     }
 
--- app/src/main/java/com/oliveira/silas/cad/ui/main/movie/MovieViewModel.kt
+++ app/src/main/java/com/oliveira/silas/cad/ui/main/movie/MovieViewModel.kt
@@ -1,9 +1,14 @@
 package com.oliveira.silas.cad.ui.main.movie
 
 import android.util.Log
+import androidx.databinding.Bindable
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableBoolean
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import com.oliveira.silas.domain.movies.Movie
 import com.oliveira.silas.domain.movies.interactor.GetPopularMoviesInteractor
+import com.oliveira.silas.domain.user.User
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.observers.DisposableMaybeObserver
@@ -11,13 +16,26 @@ import io.reactivex.observers.DisposableMaybeObserver
 class MovieViewModel(val getPopularMoviesInteractor: GetPopularMoviesInteractor) : ViewModel() {
     private val disposable = CompositeDisposable()
 
+    val loading = ObservableBoolean()
+    var result: MutableList<Movie> = ObservableArrayList<Movie>()
+    val error = ObservableField<String>()
+    val empty = ObservableBoolean()
+
 
     fun loadMovies(apiKey: String) = disposable.add(getUser(apiKey))
 
     private fun getUser(apiKey: String): Disposable {
         return getPopularMoviesInteractor.execute(getPopularMoviesInteractor.Request(apiKey))
                 .subscribeWith(object : DisposableMaybeObserver<List<Movie>>() {
+
+                    override fun onStart() {
+                        loading.set(true)
+                    }
+
                     override fun onSuccess(movie: List<Movie>) {
+                        loading.set(false)
+                        result.clear()
+                        result.addAll(movie)
                         Log.d("TESTE", "" + movie)
                     }
 
@@ -26,6 +44,7 @@ class MovieViewModel(val getPopularMoviesInteractor: GetPopularMoviesInteractor)
                     }
 
                     override fun onError(e: Throwable) {
+                        loading.set(false)
 
                         Log.d("TESTE", e.message)
                     }
