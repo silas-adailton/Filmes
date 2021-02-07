b6abda0eb772088d0bcb0e2d6a03836dd7fb223d
Silas Oliveira <silasadailton@hotmail.com>
2021-02-06 21:30:57 -0300

    Ajuste na lista. * Alteração no layout da lista.

--- app/src/main/java/br/com/oliveira/silas/movies/binding/BindingAdapters.kt
+++ app/src/main/java/br/com/oliveira/silas/movies/binding/BindingAdapters.kt
@@ -4,20 +4,24 @@ import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.bumptech.glide.Glide
 import br.com.oliveira.silas.movies.ui.main.movie.MovieAdapter
 import br.com.oliveira.silas.movies.domain.Movie
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 
-object bindAdapters {
+object BindAdapters {
 
     @JvmStatic
     @BindingAdapter("loadRecyclerViewMovies")
     fun loadRecyclerViewMovies(recyclerView: RecyclerView, listMovies: List<Movie>?) {
         val viewAdapter: RecyclerView.Adapter<MovieAdapter.ViewHolder>
-        recyclerView.layoutManager = StaggeredGridLayoutManager(2, 1)
-        recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL))
+//        recyclerView.layoutManager = StaggeredGridLayoutManager(2, 1)
+        recyclerView.layoutManager = LinearLayoutManager(recyclerView.context)
+//        recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL))
         recyclerView.setHasFixedSize(true)
 
         viewAdapter = MovieAdapter(listMovies!!)
@@ -28,7 +32,11 @@ object bindAdapters {
     @BindingAdapter("imageUrl")
     fun ImageView.setImageUrl(imageUrl: String?) {
         Glide.with(this)
-                .load("https://image.tmdb.org/t/p/w500/$imageUrl")
+                .load("https://image.tmdb.org/t/p/original/$imageUrl")
+                .apply(
+                        RequestOptions()
+                                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                )
                 .into(this)
     }
 
